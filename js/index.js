3fae750ad10ecce7414d6a5f0b83c350f961854a
devguchi <devguchitaro@gmail.com>
2021-11-06 14:05:46 +0900

    added add function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-import("../pkg/index.js").catch(console.error);
+const rust = import("../pkg/index.js");
+rust.then(m => {
+    const result = m.add(1,2); 
+    alert(`1+2=${result}`);
+}).catch(console.error);
+
