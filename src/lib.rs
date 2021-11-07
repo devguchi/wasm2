f45858c34bc6ebe19f40332ba37a14efa66b0ea2
devguchi <devguchitaro@gmail.com>
2021-11-07 13:57:09 +0900

    added canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,30 @@
+use std::f64;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 #[wasm_bindgen(start)]
-pub fn run() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no blobal 'window' exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-    let p = document.create_element("p")?;
-    p.set_text_content(Some("Hello from Rust!"));
-    body.append_child(&p)?;
-    Ok(())
+pub fn start() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+      .dyn_into::<web_sys::HtmlCanvasElement>()
+      .unwrap();
+
+    let context = canvas
+      .get_context("2d")
+      .unwrap()
+      .unwrap()
+      .dyn_into::<web_sys::CanvasRenderingContext2d>()
+      .unwrap();
+
+    context.begin_path();
+
+    context
+      .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI*2.0)
+      .unwrap();
+
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    context.stroke();
 }
